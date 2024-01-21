e2dad91888431a9982a4e2db187dc8ea5774f0d4
mmullerx4 <mmullerx4@gmail.com>
2024-01-20 22:18:07 -0500

    012024a

--- routes/contacts.js
+++ routes/contacts.js
@@ -5,5 +5,6 @@ const contactsController = require('../controllers/contacts');
 routes.get('/', contactsController.getData);
  //router.get('/', professionalController.getData);
 
+ 
 //export routes
 module.exports = routes;
--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect')
+
+
+const port = process.env.PORT || 8080;
 const app = express();
 
 
-const port = 3000;
-
+app.use('/', require('./routes'));
+app.use(bodyParser.json())
+app.use((req, res, next) => {
+    res.setHeader('Acess-Control-Allow-Origin', '*');
+    next();
+})
 app.use('/', require('./routes'));
 
-
+mongodb.initDb((err, mongodb) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port);
+        console.log('Connected to database and listening on ${port}');
+    }
+});
  
 app.listen(process.env.port || port);
 console.log('Web Server is listening at port ' + (process.env.port || port));
