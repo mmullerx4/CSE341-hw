7aa2ab2e8da0aa56c16f6d37e5fa9573abd74195
mmullerx4 <mmullerx4@gmail.com>
2024-01-30 19:45:32 -0500

    w04 team 013024

---
+++ w04-TeamAssign/routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express').Router();
+const templeRoutes = require('./temple');
+const swaggerRoutes = require('./swagger');
+ 
+express.use('/temples', templeRoutes);
+express.use('/swagger', swaggerRoutes);
+express.use(
+  '/',
+  (docData = (req, res) => {
+    let docData = {
+      documentationURL: 'https://github.com/sterlingt22/temple-api.git',
+    };
+    res.send(docData);
+  })
+);
+ 
+module.exports = express;
